15847920ac1a32ada237e5a00ac4f56f3ca421a7
Alexander Vieth <aovieth@gmail.com>
2017-02-04 21:14:59 -0500

    Connection exports bundle identifier

--- src/Network/Transport/TCP.hs
+++ src/Network/Transport/TCP.hs
@@ -744,6 +744,7 @@ apiConnect params ourEndPoint theirAddress _reliability hints =
         return Connection
           { send  = apiSend  (ourEndPoint, theirEndPoint) connId connAlive
           , close = apiClose (ourEndPoint, theirEndPoint) connId connAlive
+          , bundle = remoteId theirEndPoint
           }
 
 -- | Close a connection
@@ -1064,7 +1065,7 @@ handleIncomingMessages params (ourEndPoint, theirEndPoint) = do
                     -- report the endpoint as gone if we have any outgoing
                     -- connections
                     when (vst ^. remoteOutgoing > 0) $ do
-                      let code = EventConnectionLost (remoteAddress theirEndPoint)
+                      let code = EventConnectionLost (remoteAddress theirEndPoint) (remoteId theirEndPoint)
                       qdiscEnqueue' ourQueue theirAddr . ErrorEvent $
                         TransportError code "The remote endpoint was closed."
               removeRemoteEndPoint (ourEndPoint, theirEndPoint)
@@ -1188,7 +1189,7 @@ handleIncomingMessages params (ourEndPoint, theirEndPoint) = do
                 return (RemoteEndPointValid vst', Nothing)
               else do
                 when (vst' ^. remoteOutgoing > 0) $ do
-                  let code = EventConnectionLost (remoteAddress theirEndPoint)
+                  let code = EventConnectionLost (remoteAddress theirEndPoint) (remoteId theirEndPoint)
                   let msg  = "socket closed prematurely by peer"
                   qdiscEnqueue' ourQueue theirAddr . ErrorEvent $ TransportError code msg
                 -- Release probing resources if probing.
@@ -1219,7 +1220,7 @@ handleIncomingMessages params (ourEndPoint, theirEndPoint) = do
               else do
                 -- Release probing resources if probing.
                 when (vst ^. remoteOutgoing > 0) $ do
-                  let code = EventConnectionLost (remoteAddress theirEndPoint)
+                  let code = EventConnectionLost (remoteAddress theirEndPoint) (remoteId theirEndPoint)
                   let msg  = "socket closed prematurely by peer"
                   qdiscEnqueue' ourQueue theirAddr . ErrorEvent $ TransportError code msg
                 forM_ (remoteProbing vst) id
@@ -1278,7 +1279,7 @@ handleIncomingMessages params (ourEndPoint, theirEndPoint) = do
           RemoteEndPointValid vst -> do
             -- Release probing resources if probing.
             forM_ (remoteProbing vst) id
-            let code = EventConnectionLost (remoteAddress theirEndPoint)
+            let code = EventConnectionLost (remoteAddress theirEndPoint) (remoteId theirEndPoint)
             qdiscEnqueue' ourQueue theirAddr . ErrorEvent $ TransportError code (show err)
             return (RemoteEndPointFailed err)
           RemoteEndPointClosing resolved vst -> do
@@ -1298,7 +1299,7 @@ handleIncomingMessages params (ourEndPoint, theirEndPoint) = do
             modifyMVar_ ourState $ \st' -> case st' of
               LocalEndPointClosed -> return st'
               LocalEndPointValid _ -> do
-                let code = EventConnectionLost (remoteAddress theirEndPoint)
+                let code = EventConnectionLost (remoteAddress theirEndPoint) (remoteId theirEndPoint)
                     err  = TransportError code (show err')
                 qdiscEnqueue' ourQueue theirAddr (ErrorEvent err)
                 return st'
@@ -1528,6 +1529,7 @@ connectToSelf ourEndPoint = do
     return Connection
       { send  = selfSend connAlive connId
       , close = selfClose connAlive connId
+      , bundle = heavyweightSelfConnectionId
       }
   where
     selfSend :: IORef Bool
